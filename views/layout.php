5ba44014a7079356eef3e36d4d3b8dd1783f367a
Dugrais julien <dugrais.julien@icloud.com>
2025-01-27 21:50:03 +0100

    Ajout de la gestion des en-têtes CORS, mise à jour de la structure de la page avec un nouveau menu de navigation et ajout de nouveaux fichiers SVG pour les icônes, ainsi que la création d'un fichier CSS pour le style.

--- views/layout.php
+++ views/layout.php
@@ -5,11 +5,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Doc Troc</title>
+    <link rel="stylesheet" href="assets/init_css.css">
+    <link rel="stylesheet" href="assets/style.css">
+    <title>Tom Troc</title>
 </head>
 
 <body>
+    <header>
+        <nav>
+            <div>
+                <a href="/"><img src="assets/images/logo.svg" class="logo" alt="Logo de TomTroc"></a>
+                <ul>
+                    <li><a href="#" class="active">Accueil</a></li>
+                    <li><a href="#">Nos livres à l'échange</a></li>
+                </ul>
+            </div>
+            <ul>
+                <li class="separator"></li>
+                <li><a href="#"><img src="assets/images/icon-messagerie.svg" alt="logo message">Messagerie<span>1</span></a></li>
+                <li><a href="#"><img src="assets/images/icon-mon-compte.svg" alt="logo mon compte">Mon Compte</a></li>
+                <li><a href="#">Connexion</a></li>
+            </ul>
+        </nav>
+    </header>
     <?= $content ?>
 </body>
+<footer>
+    <nav>
+        <ul>
+            <li><a href="#">Politique de confidentialité</a></li>
+            <li><a href="#">Mentions légales</a></li>
+            <li>Tom Troc©</li>
+            <li><img src="assets/images/logo-small.svg" alt=""></li>
+        </ul>
+    </nav>
+</footer>
 
 </html>
